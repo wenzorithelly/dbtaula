364ba189223d56d60952d0f4dcec854782a684f8
Wenzo <rithellyenzo@gmail.com>
2022-11-11 03:03:22 +0000

    changes

--- models/employees.sql
+++ models/employees.sql
@@ -1,8 +1,9 @@
 with
     calc_employees as (
         select 
+            employee_id,
             date_part(year, current_date) - date_part(year, birth_date) as age,
-            date_part(year, current_date) - date_part(year, hire_date) lenght_of_service,
+            date_part(year, current_date) - date_part(year, hire_date) length_of_service,
             first_name || ' ' || last_name name
 
         from {{source('sources', 'employees')}} )
